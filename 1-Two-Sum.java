ef5e037054b879cff0a5333f0b4dad5a7047e5e0
Deepakkumar V <145677062+Deepak5556@users.noreply.github.com>
2024-12-08 13:21:37 +0530

    [1] [Time Beats: 5.02%] [Memory Beats: 7.50%] - LeetPush

---
+++ 1-Two-Sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j) {
+                    if (nums[i] + nums[j] == target) {
+                        int[] res = new int[2];
+                        res[0] = i;
+                        res[1] = j;
+                        return res;
+                    }
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
