d3ba8e7cea5efcd7e0159bddf4e8fc479e6b6a5b
Deepakkumar V <145677062+Deepak5556@users.noreply.github.com>
2024-12-08 13:23:32 +0530

    [1] [Time Beats: 98.91%] [Memory Beats: 14.00%] - LeetPush

--- 1-Two-Sum.java
+++ 1-Two-Sum.java
@@ -1,16 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i != j) {
-                    if (nums[i] + nums[j] == target) {
-                        int[] res = new int[2];
-                        res[0] = i;
-                        res[1] = j;
-                        return res;
-                    }
-                }
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
+            map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
     }
