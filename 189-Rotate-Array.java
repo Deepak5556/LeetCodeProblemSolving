db9215c8d62518ea035d0573fa150eb359b2bb06
Deepakkumar V <145677062+Deepak5556@users.noreply.github.com>
2024-12-20 11:15:55 +0530

    [189] [Time Beats: 49.70%] [Memory Beats: 5.36%] - LeetPush

--- 189-Rotate-Array.java
+++ 189-Rotate-Array.java
@@ -1,19 +1,22 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n=nums.length;
-        int j=0;
-        if(n<1)return;
+        if(k<1)return ;
         k=k%n;
-        int[] temp= new int[n];
+        int[]temp=new int[n];
+        int j=0;
+        
         for(int i=0;i<k;i++)
         {
             temp[i]=nums[n-k+i];
         }
-       for (int i = n - k - 1; i >= 0; i--) {
-            nums[i + k] = nums[i];
+        for(int i=n-k-1;i>=0;i--)
+        {
+            temp[i+k]=nums[i];
         }
-         for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=temp[i];
         }
     }
 }
