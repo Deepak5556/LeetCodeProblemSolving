bbae09d4e23984952c02f607b32015fe79ad9836
Deepakkumar V <145677062+Deepak5556@users.noreply.github.com>
2025-01-06 15:28:23 +0530

    [98] [Time Beats: 100.00%] [Memory Beats: 75.09%] - LeetPush

--- 98-Validate-Binary-Search-Tree.java
+++ 98-Validate-Binary-Search-Tree.java
@@ -22,10 +22,7 @@ class Solution {
     {
         if(root ==  null)return true;
         if(root.val <= min || root.val>=max)return false;
-        if(checkBST(root.left,min,root.val) && checkBST(root.right,root.val,max)){
-            return true;
-        }
+        if(checkBST(root.left,min,root.val) && checkBST(root.right,root.val,max))return true;
         return false;
-
     }
 }
